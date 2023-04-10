801be8e14dee9193a6daf407f48f01f120850306
AoCongHieu <letputsmile.onthatface0101@gmail.com>
2023-04-10 21:54:38 +0700

    code may tinh co ban

---
+++ MayTinhCoBan/JS/js.js
@@ -0,0 +1,15 @@
+// const result = document.getElementById('result');
+
+//     // hàm dùng để thực hiện cộng trừ nhân chia
+//     // khai báo 1 cái value để gán giá trị khi nhập từ màn hình
+//     function addPT(value) {
+//         result.value += value; // value là giá trị được gán 
+//     }
+//     // hàm dùng để thực hiện phượng thức xóa 
+//     function clearALL() {
+//         result.value = '';
+//     }
+
+//     function calculate() {
+//         result.value = eval(result.value); // eval hàm trả về giá trị của biểu thức trong nó
+//     }
